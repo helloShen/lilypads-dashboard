37e5245ebbfcda5912f0c1db06ffef1e893e8082
helloShen <symantec__@hotmail.com>
2022-02-26 00:22:43 -0500

    finish toggle sidebar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const sidebar = document.querySelector('.sidebar');
+const menu = document.querySelector('i.menu');
+menu.addEventListener('click', () => sidebar.classList.toggle('hide'));
+
+
 const stars = document.querySelectorAll('.projects > .card > .controls > i:first-child');
 
 stars.forEach((star) => {
