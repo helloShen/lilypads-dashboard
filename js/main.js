4691888f7c5640441e696b7943164f2d85362bda
helloShen <symantec__@hotmail.com>
2022-02-26 02:57:15 -0500

    finish v2.0

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const sidebar = document.querySelector('.sidebar');
+const header = document.querySelector('.header');
 const menu = document.querySelector('i.menu');
-menu.addEventListener('click', () => sidebar.classList.toggle('hide'));
+menu.addEventListener('click', () => {
+    sidebar.classList.toggle('hide');
+    header.classList.toggle('hide');
+});
 
 
 const stars = document.querySelectorAll('.projects > .card > .controls > i:first-child');
